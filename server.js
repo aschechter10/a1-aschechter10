2197956109781199f71aa79b03f98a30e86ed839
aschechter10 <aschechter03@gmail.com>
2025-01-23 18:48:44 -0500

    added all my work

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const http = require('http'),
-      fs   = require('fs'),
-      port = 3000;
+const http = require("http"),
+  fs = require("fs"),
+  port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' );
+const server = http.createServer(function (request, response) {
+  switch (request.url) {
+    case "/":
+      sendFile(response, "index.html");
       break;
-    case '/index.html':
-      sendFile( response, 'index.html' );
+    case "/index.html":
+      sendFile(response, "index.html");
+      break;
+    case "/hobby.html":
+      sendFile(response, "hobby.html");
+      break;
+    case "/styles.css":
+      sendFile(response, "styles.css", "text/css");
       break;
     default:
-      response.end( '404 Error: File Not Found' );
+      response.end("404 Error: File Not Found");
   }
 });
 
-server.listen( process.env.PORT || port );
+server.listen(process.env.PORT || port);
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' );
-   });
-}
+const sendFile = function (response, filename) {
+  fs.readFile(filename, function (err, content) {
+    response.end(content, "utf-8");
+  });
+};
